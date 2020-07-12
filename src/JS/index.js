db1642904f2af0af93f227235411e600397df65a
Alexandr Rudenko <alexandr.rudenko@protonmail.com>
2020-07-12 12:17:11 +0200

    add a working map with a marker

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,12 +1,9 @@
 import "../CSS/style.scss";
-import Map from "ol/Map";
-import View from "ol/View";
-import TileLayer from "ol/layer/Tile";
-import XYZ from "ol/source/XYZ";
-import { fromLonLat } from "ol/proj";
-
 // core version + navigation, pagination modules:
 import Swiper, { Navigation, Pagination } from "swiper";
+import L from "leaflet";
+
+
 // configure Swiper to use modules
 Swiper.use([Navigation, Pagination]);
 
@@ -28,17 +25,16 @@ let mySwiper = new Swiper(".swiper-container", {
   },
 });
 
-new Map({
-  target: "map",
-  layers: [
-    new TileLayer({
-      source: new XYZ({
-        url: "https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-      }),
-    }),
-  ],
-  view: new View({
-    center: fromLonLat([14.510302, 50.102837]),
-    zoom: 17,
-  }),
+// Create a map in div with id "map"
+ let mymap = L.map('map', {
+    center: [50.103139, 14.510206],
+    zoom: 16
 });
+
+// Use tile layer
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mymap);
+
+// add a marker to restaurant position
+let marker = L.marker([50.103139, 14.510206]).addTo(mymap);
