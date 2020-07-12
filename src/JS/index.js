31d31de728ac0845a9eefdd2fd0d3b753401dd53
Alexandr Rudenko <alexandr.rudenko@protonmail.com>
2020-07-12 12:32:36 +0200

    add go to top button with ionicon and js

--- src/JS/index.js
+++ src/JS/index.js
@@ -38,3 +38,24 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 
 // add a marker to restaurant position
 let marker = L.marker([50.103139, 14.510206]).addTo(mymap);
+
+//Get the button:
+let goupButton = document.getElementById("goup");
+goupButton.onclick = topFunction;
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    goupButton.style.display = "block";
+  } else {
+    goupButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  console.log("lol")
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
